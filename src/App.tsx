915afd7e8f67aa585b692d407a5803a38d19cbde
github akito-10 <akt.fukuda@gmail.com>
2020-12-18 21:09:44 +0900

    ぐるなびAPI取得

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,23 @@ const useStyles = makeStyles(() =>
   createStyles({
     header: {
       // backgroundColor: "#ee7800",
+      height: 50,
     },
+    main: {},
   })
 );
+
+const getCurrentPosition = () => {
+  navigator.geolocation.getCurrentPosition(fetchApi);
+};
+
+const fetchApi = async (position: any) => {
+  const res = await fetch(
+    `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=a8e3837e9f2439392dc6235ab285541a&latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&range=2`
+  );
+  const rows = await res.json();
+  console.log(rows);
+};
 
 const App: React.FC = () => {
   const classes = useStyles();
@@ -29,6 +43,9 @@ const App: React.FC = () => {
       <header className={classes.header}>
         <AppHeader handleDrawerOpen={handleDrawerOpen} />
       </header>
+      <main>
+        <p onClick={getCurrentPosition}>api</p>
+      </main>
       <nav>
         <NavDrawer open={open} handleDrawerClose={handleDrawerClose} />
       </nav>
---
+++ src/apis/getApi.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+// const app = express();
+// const bodyParser = require("body-parser");
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// app.get("/api/v1", (req, res) => {
+//   const rows = fetch(
+//     "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=a8e3837e9f2439392dc6235ab285541a"
+//   );
+//   res.json(rows.response.rest);
+// });
