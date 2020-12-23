2550878320548607e03211b0ea160882daf44fa9
github akito-10 <akt.fukuda@gmail.com>
2020-12-23 15:40:54 +0900

    いらない記述を削除

--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import { red } from "@material-ui/core/colors";
 import {
   Button,
   CardActions,
